283c4d02ee06d9e00cbbe139bcb0ab8833d966bd
Gruesome204 <lukas@logopaedie-schneider.de>
2025-05-07 05:28:02 +0200

    Updated Turret Placement

--- Assets/Scripts/PlayerScripts/TurretPlacement.cs
+++ Assets/Scripts/PlayerScripts/TurretPlacement.cs
@@ -5,7 +5,7 @@ public class TurretPlacement : MonoBehaviour
 {
     public List<TurretBlueprint> TurretBlueprintList = new List<TurretBlueprint>();
     public LayerMask groundLayer; // Assign the layer of your ground in the Inspector
-
+    public LayerMask turretLayer;
     public TurretBlueprint currentBlueprint;
     public GameObject placementPreviewPrefab; // Optional: Prefab for placement preview
     private GameObject currentPreview;
@@ -17,17 +17,15 @@ public class TurretPlacement : MonoBehaviour
             Debug.Log("Test Click");
             Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero, Mathf.Infinity, groundLayer);
-            Vector2 placementPosition = hit.point;
 
             GameObject newTurret = Instantiate(currentBlueprint.turretPrefab, worldPosition, Quaternion.identity);
+            TurretAttack turretAttack = newTurret.GetComponent<TurretAttack>();
+            if (turretAttack != null)
+            {
+                turretAttack.turretBlueprint = currentBlueprint;
+                turretAttack.InitializeFromBlueprint(); // Optional initialization method
 
-                TurretAttack turretAttack = newTurret.GetComponent<TurretAttack>();
-                if (turretAttack != null)
-                {
-                    turretAttack.turretBlueprint = currentBlueprint;
-                    turretAttack.InitializeFromBlueprint(); // Optional initialization method
-                    
-                }
+            }
         }
     }
 }
