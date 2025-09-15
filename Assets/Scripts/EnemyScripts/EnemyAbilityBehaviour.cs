420c7f09c7c5a5dc946c1708bb8af1957b505334
Gruesome204 <lukas@logopaedie-schneider.de>
2025-09-15 13:59:36 +0200

    changed enemyAbilityBehaviour exception

--- Assets/Scripts/EnemyScripts/EnemyAbilityBehaviour.cs
+++ Assets/Scripts/EnemyScripts/EnemyAbilityBehaviour.cs
@@ -18,18 +18,18 @@ public class EnemyAbilityBehaviour : MonoBehaviour
 
     private void Update()
     {
-        var runtimeList = AbilityManager.Instance.GetAbilities(gameObject);
-        if (runtimeList == null) return;
+        //var runtimeList = AbilityManager.Instance.GetAbilities(gameObject);
+        //if (runtimeList == null) return;
 
-        // Pick best ability by priority
-        var best = runtimeList
-            .Where(a => a.CanUse(gameObject, target?.gameObject))
-            .OrderByDescending(a => a.ability.priority)
-            .FirstOrDefault();
+        //// Pick best ability by priority
+        //var best = runtimeList
+        //    .Where(a => a.CanUse(gameObject, target?.gameObject))
+        //    .OrderByDescending(a => a.ability.priority)
+        //    .FirstOrDefault();
 
-        if (best != null)
-        {
-            best.Use(gameObject, target?.gameObject);
-        }
+        //if (best != null)
+        //{
+        //    best.Use(gameObject, target?.gameObject);
+        //}
     }
 }
