73b0136efc297f53a38161bb04359a4318ff3bbb
Gruesome204 <lukas@logopaedie-schneider.de>
2025-09-18 01:31:49 +0200

    Added Player Scripts

---
+++ Assets/Scripts/PlayerScripts/PlayerBehaviour.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private PlayerStats stats;
+    private PlayerHealth health;
+    private PlayerMovement movement;
+
+    private void Awake()
+    {
+        stats = GetComponent<PlayerStats>();
+        health = GetComponent<PlayerHealth>();
+        movement = GetComponent<PlayerMovement>();
+
+        stats.Initialize();
+        health.OnDeath += HandleDeath;
+    }
+    private void HandleDeath(PlayerHealth playerHealth, DamageData damageData)
+    {
+        Debug.Log($"Player died from {damageData.type} damage.");
+
+        // Destroy or disable player object
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour, IDamagable
+{
+    public PlayerStats stats;
+    public Slider healthBar;
+    public event Action<PlayerHealth, DamageData> OnDeath;
+
+    public bool IsInvulnerable { get; set; }
+
+    public void TakeDamage(DamageData damageData, KnockbackData knockbackData)
+    {
+        if (IsInvulnerable || stats == null) return;
+
+        float damage = CalculateTakenDamage(damageData);
+        stats.currentHealth -= damage;
+
+        healthBar?.SetValueWithoutNotify(stats.currentHealth / stats.currentMaxHealth);
+
+        Debug.Log($"Player took {damage} {damageData.type} damage. Remaining: {stats.currentHealth}");
+
+        if (stats.currentHealth <= 0)
+        {
+            Die(damageData);
+        }
+    }
+
+    public void Die(DamageData damageData)
+    {
+        OnDeath?.Invoke(this, damageData);
+    }
+
+    private float CalculateTakenDamage(DamageData data)
+    {
+        float dmg = data.amount;
+        switch (data.type)
+        {
+            case DamageData.DamageType.Physical:
+                dmg -= stats.currentArmor;
+                break;
+            case DamageData.DamageType.Magical:
+                float resist = stats.currentMagicResistance / 100f;
+                dmg *= (1 - Mathf.Clamp01(resist));
+                break;
+        }
+        return Mathf.Max(dmg, 0f);
+    }
+
+    public void OnDamageTaken(float amount)
+    {
+        throw new NotImplementedException();
+    }
+}
