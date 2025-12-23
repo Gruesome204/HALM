fafe3d682e72416b9626b93530a928921a64b016
Gruesome204 <lukas@logopaedie-schneider.de>
2025-12-23 19:20:47 +0100

    updated DefaultGameData

--- Assets/Scripts/ScriptableObjects/GameDataCreation/GameDataDefaultsSO.cs
+++ Assets/Scripts/ScriptableObjects/GameDataCreation/GameDataDefaultsSO.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Game/GameData/NewGameDataDefaults")]
 public class GameDataDefaultsSO : ScriptableObject
 {
-
+    [Header("Settings")]
     public string localSelected;
     public int musicTrack;
     public float masterVolume;
     public float musicVolume;
     public float soundVolume;
 
+    [Header("Player")]
     public int currentPlayerLevel;
     public GameDataSO.Class currentClass;
 
+    [Header("Resources")]
     public int gameCurrency;
     public int woodResource;
     public int steinResource;
     public int metallResource;
     public int pulverResource;
 
-    public List<TurretBlueprint> defaultUnlockedBlueprints;
-    public List<BuildMasterModifier> defaultModifiers;
+    [Header("Turrets")]
+    public List<TurretBlueprint> allTurretBlueprints;            // All turrets in the game
+    public List<TurretBlueprint> defaultUnlockedBlueprints;      // Which turrets are unlocked by default
+    public List<TurretBlueprint> defaultSelectedBlueprints;      // Optional: which are selected by default
+
+    [Header("BuildMasterModifiers")]
+    public List<BuildMasterModifier> allBuildMasterModifiers;    // All modifiers in the game
+    public List<BuildMasterModifier> defaultModifiers;           // Which modifiers are unlocked by default
+    public List<BuildMasterModifier> defaultSelectedModifiers;   // Optional: which are selected by default
 }
